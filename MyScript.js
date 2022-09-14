ffedd6b4844c02d03e61cd94fa92abeb6e828f68
luisiannarelli <30358309+luisiannarelli@users.noreply.github.com>
2022-09-14 16:55:17 -0300

    Add files via upload

--- MyScript.js
+++ MyScript.js
@@ -21,18 +21,72 @@ function getPlayerSelection () {
     return PChoice;
 }
 
+const PlayerChoice = getPlayerSelection();
+const ComputerChoice = getComputerChoice();
 
 
-function playRound (playerSelection, computerSelection) {
-    const playerSelection = getPlayerSelection();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
+
+function playround() {
+    console.log(`The player chose ${PlayerChoice}`);
+    let Pwin = false;
+    let Cwin = false;
+    let Tie = false;
+    
+    
+    if (PlayerChoice == ComputerChoice) {
+        console.log("This round is a Tie");
+        Tie = true;
+    } else if (PlayerChoice == "Rock" && ComputerChoice == "Paper"){
+        console.log("This round is for the Computer");
+        Cwin = true;
+        return Cwin;
+    } else if (PlayerChoice == "Rock" && ComputerChoice == "Scissors"){
+        console.log("This round is for the Player");
+        Pwin = true;
+        return Pwin;
+    } else if (PlayerChoice == "Paper" && ComputerChoice == "Rock"){
+        console.log("This round is for the Player");
+        Pwin = true;
+        return Pwin;
+    } else if (PlayerChoice == "Paper" && ComputerChoice == "Scissors"){
+        console.log("This round is for the Computer");
+        Cwin = true;
+        return Cwin;
+    } else if (PlayerChoice == "Scissors" && ComputerChoice == "Rock"){
+        console.log("This round is for the Computer");
+        Cwin = true;
+        return Cwin;
+    } else if (PlayerChoice == "Scissors" && ComputerChoice == "Paper"){
+        console.log("This round is for the Player");
+        Pwin = true;
+        return Pwin;
+    } 
+
+    return Pwin;
+    return Cwin;
+    return Tie;
+
 }
 
-playRound();
 
 
+function game(){
+    let CP = 0;
+    let CC = 0;
+    let CT = 0;
 
+    for (let i=0; i<5; i++){
+        playround();
+        if (Cwin == true){
+            CC = CC++;
+        }else if (Pwin == true) {
+            CP = CP++;
+        } else {
+            CT = CT++;
+        }
+    }
+}
+game();
 
 function capitalize(input) {
     input = input.toLowerCase();
